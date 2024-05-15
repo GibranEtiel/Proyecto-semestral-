d2635999833a2889425033b583761f3b0a3a8f31
Martin <Mar.camposa@duocuc.cl>
2024-05-15 16:47:13 -0400

    cambio

--- assent/js/Pago.js
+++ assent/js/Pago.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const paymentForm = document.getElementById("payment-form");
 
-    paymentForm.addEventListener("submit", function(event) {
-        event.preventDefault(); 
+const paymentForm = document.getElementById("payment-form");
 
-       
-        const nombre = document.getElementById("nombre").value;
-        const numero = document.getElementById("numero").value;
-        const fecha = document.getElementById("fecha").value;
-        const cvv = document.getElementById("cvv").value;
+paymentForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
 
        
-        if (!nombre || !numero || !fecha || !cvv) {
-            alert("Por favor, complete todos los campos.");
-            return;
-        }
+    const nombre = document.getElementById("nombre").value;
+    const numero = document.getElementById("numero").value;
+    const fecha = document.getElementById("fecha").value;
+    const cvv = document.getElementById("cvv").value;
 
-
-
-        window.location.href = "./pago-exitoso.html";
        
-    });
+    if (!nombre || !numero || !fecha || !cvv) {
+        alert("Por favor, complete todos los campos.");
+        return;
+    }
+
+
+
+    window.location.href = "./pago-exitoso.html";
+       
 });
+
 
--- assent/js/login.js
+++ assent/js/login.js
@@ -1,42 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const correo = document.getElementById("correo");
-    const contraseña = document.getElementById("contraseña");
-    const loginButton = document.getElementById("loginButton");
+
+const correo = document.getElementById("correo");
+const contraseña = document.getElementById("contraseña");
+const loginButton = document.getElementById("loginButton");
     
 
-    loginButton.addEventListener("click", function() {
-        const email = correo.value;
-        const password = contraseña.value;
+loginButton.addEventListener("click", function() {
+    const email = correo.value;
+    const password = contraseña.value;
 
       
-        if (email.trim() === "" || password.trim() === "") {
-            alert("Por favor, complete todos los campos.");
-            return;
-        }
+    if (email.trim() === "" || password.trim() === "") {
+        alert("Por favor, complete todos los campos.");
+        return;
+    }
       
 
-        window.location.href = "./index.html";
-    });
+    window.location.href = "./index.html";
 });
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    const cambiarCorreo = document.getElementById("cambiarcorreo");
-    const cambiarCorreoBoton = document.getElementById("Correobutton");
 
-    cambiarCorreoBoton.addEventListener("click", function() {
-        const correo = cambiarCorreo.value;
+
+const cambiarCorreo = document.getElementById("cambiarcorreo");
+const cambiarCorreoBoton = document.getElementById("Correobutton");
+
+cambiarCorreoBoton.addEventListener("click", function() {
+    const correo = cambiarCorreo.value;
 
        
-        if (!correo.trim()) {
-            alert("Por favor, ingrese su correo electrónico.");
-            return;
-        }
+    if (!correo.trim()) {
+        alert("Por favor, ingrese su correo electrónico.");
+        return;
+    }
 
       
-        console.log("Correo Electrónico:", correo);
-        window.location.href = "./Login.html";
+    console.log("Correo Electrónico:", correo);
+    window.location.href = "./Login.html";
 
        
-    });
 });
+
+
