22b8f2edd6434a2d517f0346a39b693654d33bba
unknown <SSDD@PNAWSA06LC1009.duoc.acad>
2024-05-30 12:35:06 -0400

    actualizacion 30

--- assent/js/carrusel-index.js
+++ assent/js/carrusel-index.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const url = 'https://my.api.mockaroo.com/users.json?key=9fdcc270';
+    const url = 'https://my.api.mockaroo.com/users.json?key=e4883c10';
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const carouselInner = document.getElementById('carousel-inner');
+            const carouselInner = document.getElementById('carousel-inner-custom');
 
             // Agrupar zapatillas en grupos de tres
             for (let i = 0; i < data.length; i += 3) {
@@ -16,11 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
                         <div class="row justify-content-center">
                             ${zapatillasGroup.map(zapatilla => `
                                 <div class="col-md-4">
-                                    <div class="card mb-4 shadow-sm" data-toggle="modal" data-target="#zapatillaModal" data-zapatilla-id="${zapatilla.id}">
+                                    <div class="card mb-4 shadow-sm" data-toggle="modal" data-target="#zapatillaModalCustom" data-zapatilla-id="${zapatilla.id}">
                                         <img src="${zapatilla.imagen}" class="card-img-top" alt="${zapatilla.nombre}">
                                         <div class="card-body">
                                             <h5 class="card-title">${zapatilla.nombre}</h5>
-                                            <p class="card-text"><b>Descripcion:</b> ${zapatilla.descripcion}</p>
                                             <p class="card-text">Tipo: ${zapatilla.tipo}</p>
                                             <p class="card-text"><b>${zapatilla.precio}</b></p>
                                         </div>
@@ -46,14 +45,14 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function mostrarDetallesZapatilla(zapatilla) {
-    const zapatillaDetalle = document.getElementById('zapatillaDetalle');
-    zapatillaDetalle.innerHTML = `
+    const zapatillaDetalleCustom = document.getElementById('zapatillaDetalleCustom');
+    zapatillaDetalleCustom.innerHTML = `
         <img src="${zapatilla.imagen}" class="img-fluid mb-3" alt="${zapatilla.nombre}">
         <h5>${zapatilla.nombre}</h5>
         <p><b>Descripcion:</b> ${zapatilla.descripcion}</p>
         <p>Tipo: ${zapatilla.tipo}</p>
         <p><b>Precio:</b> ${zapatilla.precio}</p>
     `;
-    const zapatillaModal = new bootstrap.Modal(document.getElementById('zapatillaModal'));
-    zapatillaModal.show();
+    const zapatillaModalCustom = new bootstrap.Modal(document.getElementById('zapatillaModalCustom'));
+    zapatillaModalCustom.show();
 }
---
+++ assent/js/catalogo-nino.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const url = 'https://my.api.mockaroo.com/users.json?key=e4883c10';
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const zapatillasContainer = document.getElementById('zapatillas-container');
+
+        // Mostrar zapatillas tipo "hombre" en dos filas de cuatro columnas
+        const zapatillasHombre = data.filter(zapatilla => zapatilla.tipo === 'niño');//gggggggggggggggggggggggggggggggggggggggggggggg
+        for (let i = 0; i < zapatillasHombre.length; i += 4) {
+            const zapatillasGroup = zapatillasHombre.slice(i, i + 4);
+
+            const row = document.createElement('div');
+            row.classList.add('row', 'mt-4');
+
+            zapatillasGroup.forEach(zapatilla => {
+                const col = document.createElement('div');
+                col.classList.add('col-md-3');
+
+                const card = document.createElement('div');
+                card.classList.add('card', 'mb-4', 'shadow-sm');
+                card.setAttribute('data-toggle', 'modal');
+                card.setAttribute('data-target', '#zapatillaModal');
+                card.setAttribute('data-zapatilla-id', zapatilla.id);
+
+                const img = document.createElement('img');
+                img.classList.add('card-img-top');
+                img.src = zapatilla.imagen;
+                img.alt = zapatilla.nombre;
+
+                const cardBody = document.createElement('div');
+                cardBody.classList.add('card-body');
+
+                const cardTitle = document.createElement('h5');
+                cardTitle.classList.add('card-title');
+                cardTitle.textContent = zapatilla.nombre;
+
+                const cardText = document.createElement('p');
+                cardText.classList.add('card-text');
+                cardText.innerHTML = `<b><br>Tipo: ${zapatilla.tipo}<br><b>Precio:</b> ${zapatilla.precio}`;
+
+                const comprarBtn = document.createElement('a');
+                comprarBtn.classList.add('btn', 'btn-primary');
+                comprarBtn.href = 'comprar.html';
+                comprarBtn.textContent = 'Comprar';
+
+                cardBody.appendChild(cardTitle);
+                cardBody.appendChild(cardText);
+                cardBody.appendChild(comprarBtn);
+
+                card.appendChild(img);
+                card.appendChild(cardBody);
+
+                col.appendChild(card);
+                row.appendChild(col);
+            });
+
+            zapatillasContainer.appendChild(row);
+        }
+
+        // Manejar evento de clic en una tarjeta para mostrar detalles
+        document.querySelectorAll('.card').forEach(card => {
+            card.addEventListener('click', () => {
+                const zapatillaId = card.getAttribute('data-zapatilla-id');
+                const zapatilla = zapatillasHombre.find(zapatilla => zapatilla.id === parseInt(zapatillaId));
+                mostrarDetallesZapatilla(zapatilla);
+            });
+        });
+    })
+    .catch(error => console.log(error));
+
+    // Función para mostrar los detalles de la zapatilla en el modal
+    function mostrarDetallesZapatilla(zapatilla) {
+        const modalTitle = document.getElementById('zapatillaModalLabel');
+        const modalBody = document.getElementById('zapatillaDetalle');
+        modalTitle.textContent = zapatilla.nombre;
+        modalBody.innerHTML = `
+            <img src="${zapatilla.imagen}" class="img-fluid mb-2" alt="${zapatilla.nombre}">
+            <p><b>Descripcion:</b> ${zapatilla.descripcion}</p>
+            <p>Tipo: ${zapatilla.tipo}</p>
+            <p>Precio: ${zapatilla.precio}</p>
+        `;
+    }
+});
